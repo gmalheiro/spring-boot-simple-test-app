f068acf9532c952d685389d0009009ae918cf1ff
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-09-29 15:29:08 -0300

    feat: add tightly coupled app

---
+++ test/src/main/java/com/gmalheiro/spring/boot/test/car/Corolla.java
@@ -0,0 +1,16 @@
+package com.gmalheiro.spring.boot.test.car;
+
+public class Corolla {
+    public void accelerate() {
+        System.out.println("Vroom");
+    }
+
+    public void stop() {
+        System.out.println("Stopping");
+    }
+
+
+    public void honk() {
+        System.out.println("Honking");
+    }
+}
