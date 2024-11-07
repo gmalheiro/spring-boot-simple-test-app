7b953a0cda2cb61003e56c31000b662d022b9813
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-11-07 09:57:41 -0300

    refactor: update Driver to be a component and car interface using autowired

--- test/src/main/java/com/gmalheiro/spring/boot/test/car/Driver.java
+++ test/src/main/java/com/gmalheiro/spring/boot/test/car/Driver.java
@@ -1,6 +1,12 @@
 package com.gmalheiro.spring.boot.test.car;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Driver {
+	
+		@Autowired
         private Car car;
 
         public Driver (Car car) {
